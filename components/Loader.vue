b26963ffcca10f61406217955fac47ca073ba5f5
florent.catiau <florent.catiau@veepee.com>
2020-06-29 18:52:42 +0200

    minimal reproduction of bug

---
+++ components/Loader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <!-- Removing the class attributte here fixes the problem -->
+    <div v-if="loading" class="">
+      LOADING...
+    </div>
+    <!-- Removing the v-if / v-else also fixes the problem -->
+    <slot v-else></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Loader extends Vue {
+  loading = true;
+  mounted() {
+    setTimeout(() => this.loading = false, 1000);
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="center">
+    <loader>
+      <div class="red">
+        SHOULD BE RED
+      </div>
+      <div class="blue">
+        IS BLUE
+      </div>
+    </loader>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import Loader from '@/components/Loader.vue';
+
+@Component({
+  components: {
+    Loader,
+  }
+})
+export default class Page extends Vue {
+}
+</script>
+
+<style scoped style="css">
+.center {
+  text-align: center;
+}
+.red {
+  color: red;
+}
+.blue {
+  color: blue;
+}
+.green {
+  color: green;
+}
+</style>
